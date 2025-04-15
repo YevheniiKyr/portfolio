a33a360947a6074c8a083102afe7d182245dfefa
YevheniiKyr <jekafekz@gmail.com>
2025-04-15 16:52:44 +0300

    next try)

--- client/functions/api.js
+++ client/functions/api.js
@@ -9,10 +9,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 5000
-
 app.post('/send-message', send);
-
-app.listen(PORT, ()=>console.log("server is started on Port", PORT))
 
 module.exports.handler = serverless(app)
